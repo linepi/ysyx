ff8b1a0988b1cfe3565c7f2b0c8549daa401aef0
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-15 20:19:14 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:19:14 up  9:43,  3 users,  load average: 0.32, 0.39, 0.38

---
+++ npc/csrc/sim_main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+ 
+#include "Vour.h"  // create `top.v`,so use `Vtop.h`
+#include "verilated.h"
+ 
+#include "verilated_vcd_c.h" //可选，如果要导出vcd则需要加上
+ 
+int main(int argc, char** argv, char** env) {
+ 
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vour* our = new Vour{contextp};
+  
+ 
+  VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
+  contextp->traceEverOn(true); //打开追踪功能
+  our->trace(tfp, 0); //
+  tfp->open("wave.vcd"); //设置输出的文件wave.vcd
+ 
+ 
+  while (!contextp->gotFinish()) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    our->a = a;
+    our->b = b;
+    our->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, our->f);
+ 
+    tfp->dump(contextp->time()); //dump wave
+    contextp->timeInc(1); //推动仿真时间
+ 
+    assert(our->f == a ^ b);
+  }
+  delete our;
+  tfp->close();
+  delete contextp;
+  return 0;
+}
