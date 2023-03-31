a8a3072dd24a8d2fcf6de9ee2176078f793f4522
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-31 17:07:32 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:07:32 up 16:36,  1 user,  load average: 0.35, 0.45, 0.33

--- npc/auto_bind.cpp
+++ npc/auto_bind.cpp
@@ -2,4 +2,7 @@
 #include "Vtop.h"
 
 void nvboard_bind_all_pins(Vtop* top) {
+	nvboard_bind_pin( &top->a, BIND_RATE_RT , BIND_DIR_OUT, 1, VGA_VSYNC);
+	nvboard_bind_pin( &top->b, BIND_RATE_RT , BIND_DIR_OUT, 1, VGA_HSYNC);
+	nvboard_bind_pin( &top->f, BIND_RATE_RT , BIND_DIR_OUT, 1, VGA_BLANK_N);
 }
