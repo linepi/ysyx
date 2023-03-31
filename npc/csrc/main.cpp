156583fdb92c7347a2739a237e6c337979cec73c
吴桐 <mmagicode@gmail.com>
2023-03-31 17:23:48 +0800

    successfully connect nvboard

---
+++ npc/csrc/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+ 
+#include <Vtop.h>  
+#include <nvboard.h>
+ 
+void nvboard_bind_all_pins(Vtop* top);
+ 
+int main(int argc, char** argv, char** env) {
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vtop* top = new Vtop{contextp};
+  
+  nvboard_bind_all_pins(top);
+  nvboard_init();
+ 
+  while (!contextp->gotFinish()) {
+    top->eval();
+    nvboard_update();
+  }
+  delete top;
+  delete contextp;
+  nvboard_quit();
+  return 0;
+}
