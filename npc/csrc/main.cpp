9a7a8fa2c8bd6e50a0dfd3cca7d583742f54b1df
吴桐 <mmagicode@gmail.com>
2023-04-03 10:28:55 +0800

    4-3-start

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -4,19 +4,32 @@
  
 #include <Vtop.h>  
 #include <nvboard.h>
+
+Vtop *top;
+
+void single_cycle() {
+  top->clk = 0; top->eval();
+  top->clk = 1; top->eval();
+}
+
+void reset(int n) {
+  top->rst = 1;
+  while (n-- > 0) single_cycle();
+  top->rst = 0;
+}
  
 void nvboard_bind_all_pins(Vtop* top);
  
 int main(int argc, char** argv, char** env) {
   VerilatedContext* contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
-  Vtop* top = new Vtop{contextp};
+  top = new Vtop{contextp};
   
   nvboard_bind_all_pins(top);
   nvboard_init();
  
   while (!contextp->gotFinish()) {
-    top->eval();
+    single_cycle();
     nvboard_update();
   }
   delete top;
