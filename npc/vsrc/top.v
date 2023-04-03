c2a715b4c4430b59ad1c7f965223475dda6a8f1c
吴桐 <mmagicode@gmail.com>
2023-04-03 11:32:17 +0800

    NJU lab 1

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,14 +1,16 @@
-module top(
-  input clk,
-  input rst,
-  output reg [15:0] led
-);
-  reg [31:0] count;
-  always @(posedge clk) begin
-    if (rst) begin led <= 1; count <= 0; end
-    else begin
-      if (count == 0) led <= {led[14:0], led[15]};
-      count <= (count >= 50000 ? 32'b0 : count + 1);
-    end
-  end
+module top(x1, x2, x3, x4, s, y);
+  input [1:0] x1;
+  input [1:0] x2;
+  input [1:0] x3;
+  input [1:0] x4;
+  input [1:0] s;
+  output reg [1:0] y;
+  always @ (s or x1 or x2 or x3 or x4)
+    case (s)
+      2'b00: y = x1;
+      2'b01: y = x2;
+      2'b10: y = x3;
+      2'b11: y = x4;
+      default: y = 2'b00;
+    endcase
 endmodule
