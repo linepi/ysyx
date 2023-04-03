9c83e7ca7b82f8a100f2fc7e2c1fdaae2edd4cd4
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 20:21:26 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:21:26 up 10:00,  1 user,  load average: 0.55, 0.39, 0.47

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -7,7 +7,7 @@ module top(x, y, seg1, seg0);
   wire [3:0] s;
   wire cout;
   adder #(4) adder_ins(.a(x), .b(y), .cin(0), .s(s), .cout(cout));
-  encode_seg enc(.x(s), .y(seg0));
-  encode_seg enc(.x(cout), .y(seg1));
+  encode_seg enc1(.x(s), .y(seg0));
+  encode_seg enc2(.x(cout), .y(seg1));
 endmodule
 
