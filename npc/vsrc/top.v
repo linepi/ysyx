2f3f8178f6fa0ec8adb59001444a5393bf170964
吴桐 <mmagicode@gmail.com>
2023-04-03 11:41:13 +0800

    NJU LAB 1

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,16 +1,14 @@
-module top(x1, x2, x3, x4, s, y);
-  input [1:0] x1;
-  input [1:0] x2;
-  input [1:0] x3;
-  input [1:0] x4;
-  input [1:0] s;
-  output reg [1:0] y;
-  always @ (s or x1 or x2 or x3 or x4)
-    case (s)
-      2'b00: y = x1;
-      2'b01: y = x2;
-      2'b10: y = x3;
-      2'b11: y = x4;
-      default: y = 2'b00;
-    endcase
+module top(x1,x2,x3,x4,s,y);
+  input  [1:0] x1;
+  input  [1:0] x2;
+  input  [1:0] x3;
+  input  [1:0] x4;
+  input  [1:0] s;
+  output [1:0] y;
+  MuxKeyWithDefault #(4, 2, 2) i0 (y, s, 2'b00, {
+    2'b00, x1,
+    2'b01, x2,
+    2'b10, x3,
+    2'b11, x4
+  });
 endmodule
