2366266d451d5554634e2ba187dda1534da04e48
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 11:29:58 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:29:58 up  1:09,  1 user,  load average: 0.18, 0.40, 0.53

--- npc/auto_bind.cpp
+++ npc/auto_bind.cpp
@@ -2,8 +2,10 @@
 #include "Vtop.h"
 
 void nvboard_bind_all_pins(Vtop* top) {
-	nvboard_bind_pin( &top->a, BIND_RATE_SCR, BIND_DIR_IN , 1, SW0);
-	nvboard_bind_pin( &top->b, BIND_RATE_SCR, BIND_DIR_IN , 1, SW1);
-	nvboard_bind_pin( &top->s, BIND_RATE_SCR, BIND_DIR_IN , 1, SW2);
-	nvboard_bind_pin( &top->y, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD0);
+	nvboard_bind_pin( &top->x1, BIND_RATE_SCR, BIND_DIR_IN , 2, SW1, SW0);
+	nvboard_bind_pin( &top->x2, BIND_RATE_SCR, BIND_DIR_IN , 2, SW3, SW2);
+	nvboard_bind_pin( &top->x3, BIND_RATE_SCR, BIND_DIR_IN , 2, SW5, SW4);
+	nvboard_bind_pin( &top->x4, BIND_RATE_SCR, BIND_DIR_IN , 2, SW7, SW6);
+	nvboard_bind_pin( &top->s, BIND_RATE_SCR, BIND_DIR_IN , 2, SW9, SW8);
+	nvboard_bind_pin( &top->y, BIND_RATE_SCR, BIND_DIR_OUT, 2, LD1, LD0);
 }
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,6 +1,16 @@
-module top(
-  input a,b,s,
-  output y
-);
-  assign  y = (~s&a)|(s&b);
+module top(x1, x2, x3, x4, s, y);
+  input [1:0] x1;
+  input [1:0] x2;
+  input [1:0] x3;
+  input [1:0] x4;
+  input [1:0] s;
+  output [1:0] y;
+  always @ (s or x1 or x2 or x3 or x4)
+    case (s)
+      2'b00: y = x1;
+      2'b01: y = x2;
+      2'b10: y = x3;
+      2'b11: y = x4;
+      default: y = 2'b00;
+    endcase
 endmodule
