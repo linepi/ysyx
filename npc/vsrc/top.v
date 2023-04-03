b26bce7d281b70eb9082a4ea87770448474a8d57
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 20:04:31 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:04:31 up  9:43,  1 user,  load average: 0.51, 0.52, 0.61

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,19 +1,3 @@
-module encode83(x, en, y);
-  input [7:0] x;
-  input en;
-  output reg [2:0] y;
-  integer i;
-
-  always @(x or en) begin
-    y = 0;
-    if (en) begin
-      for (i = 0; i < 8; i = i + 1) 
-        if (x[i] == 1) y = i[2:0];
-    end
-    else y = 0;
-  end 
-endmodule
-
 module top(x, en, led, flag, seg);
   input [7:0] x;
   input en;
@@ -25,7 +9,7 @@ module top(x, en, led, flag, seg);
     if (x != 0 && en) flag = 1;
     else flag = 0;
   end
-  encode83 enc83(.x(x), .en(en), .y(led));
+  prior_encoder #(8, 3) encoder83(.x(x), .en(en), .y(led));
   encode_seg enc_seg(.x({1'b0, led}), .y(seg));
 endmodule
 
