6c8c639011c25133c91d9f63843ba7fe26cc9302
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 17:28:08 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:28:08 up  7:07,  1 user,  load average: 0.31, 0.38, 0.48

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -2,14 +2,12 @@ module top(x,en,y);
   input  [2:0] x;
   input  en;
   output reg [7:0]y;
-  integer i;
+  integer [2:0] i;
 
   always @(x or en)
     if (en) begin
       for( i = 0; i <= 7; i = i+1)
-       /* verilator lint_off WIDTHEXPAND */ // 添加此行
           if(x == i)
-          /* verilator lint_on WIDTHEXPAND */ // 添加此行
                 y[i] = 1;
           else
                 y[i] = 0;
