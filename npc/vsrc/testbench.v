e74aebc087891faf6915be67078a3cfcbcfaf6de
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 18:36:17 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:36:17 up 1 day,  8:15,  1 user,  load average: 0.34, 0.55, 0.53

--- npc/vsrc/testbench.v
+++ npc/vsrc/testbench.v
@@ -8,27 +8,3 @@ module testbench;
     #10;
   end
 endmodule
-
-module counter(
-  input clk,
-  output reg Q0,
-  output reg Q1,
-  output reg Q2
-);
-  wire w1, w2, w3;
-  dff dff_inst1(.clk(clk), .D(w1), .Q(Q0), ._Q(w1));
-  dff dff_inst2(.clk(w1), .D(w2), .Q(Q1), ._Q(w2));
-  dff dff_inst3(.clk(w2), .D(w3), .Q(Q2), ._Q(w3));
-endmodule
-
-module dff(
-  input clk,
-  input D,
-  output reg Q,
-  output reg _Q
-);
-  always @(posedge clk) begin
-    Q <= D;
-    _Q <= ~D; 
-  end
-endmodule
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -4,8 +4,5 @@ module top(
   output reg Q1,
   output reg Q2
 );
-  wire w1, w2, w3;
-  dff dff_inst1(.clk(clk), .D(w1), .Q(Q0), ._Q(w1));
-  dff dff_inst2(.clk(w1), .D(w2), .Q(Q1), ._Q(w2));
-  dff dff_inst3(.clk(w2), .D(w3), .Q(Q2), ._Q(w3));
+  counter counter_ins(.clk(clk), .Q0(Q0), .Q1(Q1), .Q2(Q2));
 endmodule
