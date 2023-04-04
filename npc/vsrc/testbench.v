fa729c2a8ad58ac56f11f386e1bf9d6c79b18132
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 19:44:35 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:44:35 up 1 day,  9:23,  1 user,  load average: 0.37, 0.46, 0.43

--- npc/vsrc/testbench.v
+++ npc/vsrc/testbench.v
@@ -1,16 +1,12 @@
 module testbench;
-  reg clk, Q0, Q1, Q2;
-  counter counter_ins(clk, Q0, Q1, Q2);
-  
-  reg [31:0] count_clk;
+  reg clk;
+  reg [7:0] res;
   always#10 clk = ~clk;
-
+  random_8 random_8_ins(.clk(clk), .res(res));
+  initial begin
+    clk = 0;
+  end
   always @(posedge clk) begin
-    if (count_clk == 249999) begin
-      count_clk <= 0;
-    end 
-    else begin
-      count_clk <= count_clk + 1;
-    end
+    $display(res);
   end
 endmodule
