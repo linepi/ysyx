b3d25bc46bf630477f43cf633092b3ac2503d151
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 19:40:10 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:40:10 up 1 day,  9:19,  1 user,  load average: 0.77, 0.41, 0.39

--- npc/vsrc/lab6.v
+++ npc/vsrc/lab6.v
@@ -1,7 +1,7 @@
 // 随机数发生器
 module random_8(
-    input clk;
-    output reg [7:0] res;
+    input clk,
+    output reg [7:0] res
 );
     always@(posedge clk) begin
         assign newbit <= res[0] ^ res[1] ^ res[4];
