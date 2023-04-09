0351d26c20289c8b85286eb44cb3629ee9942d9a
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 11:25:54 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:25:54 up  1:24,  1 user,  load average: 0.42, 0.41, 0.47

--- npc/vsrc/nb.v
+++ npc/vsrc/nb.v
@@ -13,6 +13,7 @@ module nb(
   output reg [7:0] seg7
 );
   wire [7:0] code;
+  wire [7:0] ascii;
   reg enable;
   initial begin 
     seg0[7] = 1;
@@ -27,11 +28,34 @@ module nb(
   end
 
   keyboard keyboard_ins(clk, resetn, ps2_clk, ps2_data, code);
+  ascii_map ascii_map_ins(code, ascii);
   encode_seg encode_seg_ins0(code[3:0], enable, seg0[6:0]);
   encode_seg encode_seg_ins1(code[7:4], enable, seg1[6:0]);
+  encode_seg encode_seg_ins2(ascii[3:0], enable, seg2[6:0]);
+  encode_seg encode_seg_ins3(ascii[7:4], enable, seg3[6:0]);
   always @(posedge clk) begin
     if(code == 8'hf0) begin
       enable = 0;
     end
   end
+endmodule
+
+// number's ascii
+module ascii_map (
+  input [7:0] code,
+  output [7:0] ascii
+);
+  case (code)
+    8'h16: ascii = 8'h31; 
+    8'h1e: ascii = 8'h32; 
+    8'h26: ascii = 8'h33; 
+    8'h25: ascii = 8'h34; 
+    8'h2e: ascii = 8'h35; 
+    8'h36: ascii = 8'h36; 
+    8'h3d: ascii = 8'h37; 
+    8'h3e: ascii = 8'h38; 
+    8'h46: ascii = 8'h39; 
+    8'h45: ascii = 8'h30; 
+    default: ascii = 8'hff;
+  endcase 
 endmodule
