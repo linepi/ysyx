909763777c449fad4c75bea87f2106e087814f50
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 11:10:09 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:10:09 up  1:08,  1 user,  load average: 0.41, 0.57, 0.62

--- npc/vsrc/nb.v
+++ npc/vsrc/nb.v
@@ -9,13 +9,15 @@ module nb(
   // output reg [6:0] seg3;
 );
   wire [7:0] code;
+  reg enable;
+  initial enable = 1;
+
   keyboard keyboard_ins(clk, resetn, ps2_clk, ps2_data, code);
-  encode_seg encode_seg_ins0(code[3:0], seg0);
-  encode_seg encode_seg_ins1(code[7:4], seg1);
+  encode_seg encode_seg_ins0(code[3:0], enable, seg0);
+  encode_seg encode_seg_ins1(code[7:4], enable, seg1);
   always @(posedge clk) begin
     if(code == 8'hf0) begin
-      seg0 = 7'b1111111;
-      seg1 = 7'b1111111;
+      enable = 0;
     end
   end
 endmodule
