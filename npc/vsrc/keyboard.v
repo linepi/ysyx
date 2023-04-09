d24cab606152cf85d76d8af9364d834cf1529e93
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 10:25:40 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:25:40 up 24 min,  1 user,  load average: 0.57, 0.89, 0.65

--- npc/vsrc/keyboard.v
+++ npc/vsrc/keyboard.v
@@ -7,7 +7,7 @@ module keyboard(clk,resetn,ps2_clk,ps2_data);
 
     always @(posedge clk) begin
         ps2_clk_sync <=  {ps2_clk_sync[1:0],ps2_clk};
-        if(ps2_clk_sync) $display("%b", ps2_clk_sync);
+        if(ps2_clk_sync > 0) $display("%b", ps2_clk_sync);
     end
 
     wire sampling = ps2_clk_sync[2] & ~ps2_clk_sync[1];
