48bec4a8a549297250fc58d30ff778a79555292d
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-11 14:07:59 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:07:59 up  4:36,  1 user,  load average: 0.77, 0.53, 0.38

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -26,7 +26,7 @@
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 */
 #define Log(format, ...) \
-    _Log(ANSI_FMT("[%s:%d %s] ", ANSI_FG_BLUE) "\n" format, \
+    _Log(ANSI_FMT("[%s:%d %s] ", ANSI_FG_BLUE) format"\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 
 #define Assert(cond, format, ...) \
