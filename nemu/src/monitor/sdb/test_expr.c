981cac6301934f9c5632d391bf1df81f8526d92e
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-13 21:28:26 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:28:26 up 13:37,  1 user,  load average: 0.68, 0.61, 0.60

--- nemu/src/monitor/sdb/test_expr.c
+++ nemu/src/monitor/sdb/test_expr.c
@@ -4,7 +4,7 @@
 // 121+((216-818)*819*298)*302
 
 void test_expr() {
-    FILE* fp = fopen("/home/w/dev/ysyx-workbench/nemu/tools/gen-expr/build/test1", "r");
+    FILE* fp = fopen("/home/w/dev/ysyx-workbench/nemu/tools/gen-expr/build/test0", "r");
     assert(fp != NULL);
     expr_t expect;
     char e[20000];
