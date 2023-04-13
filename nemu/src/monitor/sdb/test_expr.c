9e19868e00a1618d728ca3f4b2bb05d362798ada
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-13 20:44:28 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:44:28 up 12:53,  1 user,  load average: 0.50, 0.63, 0.62

--- nemu/src/monitor/sdb/test_expr.c
+++ nemu/src/monitor/sdb/test_expr.c
@@ -1,6 +1,22 @@
 #include <common.h>
-sword_t expr(char *e, bool *success);
+#include <debug.h>
+#include "sdb.h"
 
 void test_expr() {
-    expr(NULL, NULL);
+    FILE* fp = fopen("/home/w/dev/ysyx-workbench/nemu/tools/gen-expr/build/test0", "r");
+    assert(fp != NULL);
+    long expect;
+    char e[2000];
+    while (fscanf(fp, "%ld", &expect) != EOF && fscanf(fp, "%s", e) != EOF) {
+        bool success;
+        sword_t res = expr(e, &success);
+        if (!success) {
+            printf("expr fail for %s\n", e);
+            continue;
+        }
+        if (res != expect) {
+            printf("unsame for %s\nexpect: %ld, res: %ld\n", e, expect, res);
+        }
+    }
+    fclose(fp);
 }
