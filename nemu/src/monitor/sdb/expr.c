f0cef8d40facee7c26755448a12c7d6d0b1fd6a1
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-13 18:13:13 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:13:13 up 10:21,  1 user,  load average: 0.56, 0.42, 0.38

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -160,7 +160,7 @@ static bool check_parentheses(int p, int q, bool *status) {
       }
       // match
       if (stack[stacktop] == '(') {
-        if (p == q && tokens[p].type == '(') return true;
+        if (p == q && tokens[store_p].type == '(') return true;
         stacktop--;
       }
     }
@@ -189,7 +189,7 @@ static int get_main_operator(int p, int q) {
     if (tokens[p].type == '(') leftn++;
     if (tokens[p].type == ')') leftn--;
     if ((tokens[p].type <= TK_RIGHT && tokens[p].type >= TK_EQ) || 
-        (tokens[p].type < 256 && (tokens[p].type != '(' && tokens[p].type != ')'))) {
+        (tokens[p].type < 256 && tokens[p].type != '(' && tokens[p].type != ')')) {
       int tmp = get_priority(tokens[p].type); 
       if (tmp <= priority && leftn == 0) {
         priority = tmp;
