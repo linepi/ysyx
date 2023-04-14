0cf47a6e8ddab91810db66bf7c9490583e7a0b00
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-14 10:03:25 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:03:25 up 10:47,  1 user,  load average: 0.82, 0.79, 0.87

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -95,13 +95,30 @@ static int cmd_x(char *args) {
 }
 
 static int cmd_p(char *args) {
+  char *first = strtok(args, " ");
+  char *second = strtok(NULL, " ");
+  char *e = first;
+  if (second != NULL) {
+    if (strcmp(first, "hex") != 0) {
+      Error("Invalid command.\n");
+      printf("Usage: p [hex] <expression>. example: p $s0 + 5 \n");
+      return 0;
+    }
+    e = second;
+  }
+
   bool success;
-  expr_t val = expr(args, &success);
+  expr_t val = expr(e, &success);
   if (!success) {
     Error("Invalid Expression\n");
     return 0;
   }
-  printf(EXPR_NUM_FMT"\n", val);
+
+  if (second == NULL) {
+    printf(EXPR_NUM_FMT"\n", val);
+  } else {
+    printf("%lx\n", val);
+  }
   return 0;
 }
 
@@ -118,7 +135,7 @@ static struct {
   { "si", "Usage: si [N]. Step N instruction, default 1. ", cmd_si },
   { "info", "Usage: info <r|w>. r --> register, w --> watch points. ", cmd_info },
   { "x", "Usage: x <number of bytes> <expression>. example: x 10 0x80000000 ", cmd_x },
-  { "p", "Usage: p <expression>. example: p $s0 + 5 ", cmd_p },
+  { "p", "Usage: p [hex] <expression>. example: p $s0 + 5 ", cmd_p },
 
 };
 
