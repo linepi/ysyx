58037c9c758b18543d8bcac988663904c64e5109
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 13:39:24 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:39:24 up  3:10,  1 user,  load average: 0.82, 0.56, 0.45

--- nemu/scripts/count.mk
+++ nemu/scripts/count.mk
@@ -1,6 +1,6 @@
 COUNT_SRC = $(shell find ./src -name *.c -o -name *.h | xargs wc -l | grep total)
 COUNT_SRC := $(subst \n,,$(COUNT_SRC))
-COUNT_TEST = $(shell find ./tests -name *.c | xargs wc -l | grep total)
+COUNT_TEST = $(shell find ./tests -name *.c | xargs wc -l | tail -n 1 | awk '/[0-9]+/')
 COUNT_TEST := $(subst \n,,$(COUNT_TEST))
 COUNT_INCLUDE = $(shell find ./include -name *.h | xargs wc -l | grep total) 
 COUNT_INCLUDE := $(subst \n,,$(COUNT_INCLUDE))
