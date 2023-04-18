20ec71a0f2deb4c1428685fb92c7ec4cdbf15d24
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-18 12:16:56 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:16:56 up  3:34,  1 user,  load average: 0.66, 0.48, 0.34

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -19,13 +19,14 @@ extern uint64_t g_nr_guest_inst;
 FILE *log_fp = NULL;
 
 void init_log(const char *log_file) {
-  log_fp = stdout;
-  if (log_file != NULL) {
-    FILE *fp = fopen(log_file, "w");
-    Assert(fp, "Can not open '%s'", log_file);
-    log_fp = fp;
-  }
-  Log("Log is written to %s", log_file ? log_file : "stdout");
+  // log_fp = stdout;
+  // if (log_file != NULL) {
+  //   FILE *fp = fopen(log_file, "w");
+  //   Assert(fp, "Can not open '%s'", log_file);
+  //   log_fp = fp;
+  // }
+  if (log_fp)
+    Log("Log is written to %s", log_file ? log_file : "stdout");
 }
 
 bool log_enable() {
