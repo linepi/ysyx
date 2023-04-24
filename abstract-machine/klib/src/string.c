3a28d9b2e1830be059373333412287bf515bf047
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-24 16:47:37 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:47:37 up 1 day,  8:21,  1 user,  load average: 0.40, 0.31, 0.33

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -33,11 +33,12 @@ char *strcat(char *dst, const char *src) {
 
 int strcmp(const char *s1, const char *s2)
 {
-  while (*s1){
-    if (*s1 != *s2) break;
-    s1++; s2++;
-  }
-  return *(const unsigned char*)s1 > *(const unsigned char*)s2;
+  panic("");
+  // while (*s1){
+  //   if (*s1 != *s2) break;
+  //   s1++; s2++;
+  // }
+  // return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
