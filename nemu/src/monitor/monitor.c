222031223e1eb20231dbf94e414a244cd100e087
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-25 11:29:19 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:29:19 up 2 days,  3:02,  1 user,  load average: 0.90, 0.84, 0.84

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -27,6 +27,7 @@ void init_disasm(const char *triple);
 
 static void welcome() {
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
+  IFDEF(CONFIG_CC_ASAN, Log("Address sanitizer is enabled"));
   IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
         "to record the trace. This may lead to a large log file. "
         "If it is not necessary, you can disable it in menuconfig"));
