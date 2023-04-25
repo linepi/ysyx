97035350f22a10cd5f8e5ad4e76ff1f74f98dd17
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-25 19:39:52 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:39:52 up 2 days, 11:13,  1 user,  load average: 0.45, 0.60, 0.71

--- nemu/src/monitor/sdb/trace.c
+++ nemu/src/monitor/sdb/trace.c
@@ -26,9 +26,9 @@ void make_functbl() {
     }
   }
   functbl[idx].end = true;
-  // for (int i = 0; !functbl[i].end; i++) {
-  //   printf("func addr 0x%016lx, func name %s\n", functbl[i].addr, functbl[i].name);
-  // }
+  for (int i = 0; !functbl[i].end; i++) {
+    printf("func addr 0x%016lx, func name %s\n", functbl[i].addr, functbl[i].name);
+  }
 }
 
 void ftrace(vaddr_t pc) {
