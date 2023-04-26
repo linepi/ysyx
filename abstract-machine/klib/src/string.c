d2442a691e494a7707b58a134486a31e62dd750b
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-26 12:29:19 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:29:19 up 3 days,  4:02,  1 user,  load average: 1.06, 0.85, 0.72

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,14 +5,13 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  size_t len = 0;
-  while (*s++) len++;
-  return len;
+  const char *start = s;
+  while (*s++);
+  return s - start - 1;
 }
 
 char *strcpy(char *dst, const char *src) {
   char *save = dst;
-
 	for (; (*dst = *src); ++dst, ++src);
 	return save;
 }
