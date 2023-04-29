8ac5805b03c197bcca28f68124d15734a76eeb52
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-29 11:53:02 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:53:02 up 53 min,  1 user,  load average: 0.33, 0.29, 0.33

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -36,57 +36,86 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
       out++;
       fmt++;
     } else {
-      switch (*(fmt+1)) 
-      {
-      case 's':
-        char *str = va_arg(ap, char *);
-        if (valid) strcpy(out, str);
-        out += strlen(str);
-        break;
-      case 'c':
-        if (valid) *out = va_arg(ap, int);
-        out++;
-        break;
-      case 'd':
-        int d = va_arg(ap, int);
-        if (valid) out += itoa(d, out, 10);
-        else out += INTEGER_LEN(d, 10);
-        break;
-      case 'u':
-        unsigned int u = va_arg(ap, unsigned int);
-        if (valid) out += utoa(u, out, 10);
-        else out += INTEGER_LEN(u, 10);
-        break;
-      case 'x':
-        unsigned int x = va_arg(ap, unsigned int);
-        if (valid) out += utoa(x, out, 16);
-        else out += INTEGER_LEN(x, 16);
-        break;
-      case 'l':
-        switch (*(fmt+2))
-        {
+      fmt++;
+      char pre = '\0';
+      int nr_pre = 0;
+      if (*fmt == '0') {
+        pre = '0';
+        fmt++;
+      }
+      else if (*fmt <= '9' && *fmt >= '1') pre = ' ';
+      while (*fmt <= '9' && *fmt >= '1') {
+        nr_pre *= 10;
+        nr_pre += *fmt - '0';
+        fmt++;
+      }
+      char *saved_out = out;
+
+      switch (*fmt) {
+        case 's':
+          char *str = va_arg(ap, char *);
+          if (valid) strcpy(out, str);
+          out += strlen(str);
+          break;
+        case 'c':
+          if (valid) *out = va_arg(ap, int);
+          out++;
+          break;
         case 'd':
-          long int ld = va_arg(ap, long int);
-          if (valid) out += ltoa(ld, out, 10);
-          else out += INTEGER_LEN(ld, 10);
+          int d = va_arg(ap, int);
+          if (valid) out += itoa(d, out, 10);
+          else out += INTEGER_LEN(d, 10);
           break;
         case 'u':
-          unsigned long lu = va_arg(ap, unsigned long);
-          if (valid) out += ultoa(lu, out, 10);
-          else out += INTEGER_LEN(lu, 10);
+          unsigned int u = va_arg(ap, unsigned int);
+          if (valid) out += utoa(u, out, 10);
+          else out += INTEGER_LEN(u, 10);
           break;
         case 'x':
-          unsigned long lx = va_arg(ap, unsigned long);
-          if (valid) out += ultoa(lx, out, 16);
-          else out += INTEGER_LEN(lx, 16);
+          unsigned int x = va_arg(ap, unsigned int);
+          if (valid) out += utoa(x, out, 16);
+          else out += INTEGER_LEN(x, 16);
           break;
+        case 'l':
+          switch (*(fmt+2)) {
+            case 'd':
+              long int ld = va_arg(ap, long int);
+              if (valid) out += ltoa(ld, out, 10);
+              else out += INTEGER_LEN(ld, 10);
+              break;
+            case 'u':
+              unsigned long lu = va_arg(ap, unsigned long);
+              if (valid) out += ultoa(lu, out, 10);
+              else out += INTEGER_LEN(lu, 10);
+              break;
+            case 'x':
+              unsigned long lx = va_arg(ap, unsigned long);
+              if (valid) out += ultoa(lx, out, 16);
+              else out += INTEGER_LEN(lx, 16);
+              break;
+            case 'f':
+            default: panic("Not implemented");
+          }
+          fmt += 1;
+          break;
+        case 'f':
         default: panic("Not implemented");
-        }
-        fmt += 1;
-        break;
-      default: panic("Not implemented");
       }
-      fmt += 2;
+      int added_len = saved_out - out;
+      if (nr_pre > added_len) {
+        if (valid) {
+          // move string forward nr_pre - added_len char
+          for (int i = added_len - 1; i >= 0; i--) {
+            saved_out[i + nr_pre - added_len] = saved_out[i]; 
+          }
+          // fill pre
+          for (int i = 0; i < nr_pre - added_len; i++) {
+            saved_out[i] = pre;
+          }
+        } 
+        out = saved_out + nr_pre;
+      }
+      fmt += 1;
     }
   }
   if (valid) *out = '\0';
