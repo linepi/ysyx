3608a526d8672a75b8f2959d4cbdcfff33135096
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-05 16:30:08 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:30:08 up  7:23,  1 user,  load average: 0.42, 0.44, 0.48

--- npc/vsrc/LemonPC/control.v
+++ npc/vsrc/LemonPC/control.v
@@ -4,7 +4,7 @@ module control (
 );
   always @(*) begin
     case (inst)
-      32'b0000000_00001 00000 000 00000 1110011: ebreak = 1'b1;
+      32'b0000000_00001_00000_000_00000_1110011: ebreak = 1'b1;
       default: ebreak = 1'b0;
     endcase
   end
