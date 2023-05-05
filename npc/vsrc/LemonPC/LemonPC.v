5e00dfe6de00d8c09873bac8d46ace2159512929
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-05 14:47:59 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:47:59 up  5:41,  1 user,  load average: 0.82, 0.52, 0.42

--- npc/vsrc/LemonPC/LemonPC.v
+++ npc/vsrc/LemonPC/LemonPC.v
@@ -4,13 +4,14 @@ module PC (
   output reg [63:0] pc
 );
   wire [4:0] rs1 = inst[19:15];
+  wire [4:0] rs2 = 0;
   wire [4:0] rd = inst[11:7];
   // signed extend
   wire [63:0] imm = {{52{inst[31]}}, inst[31:20]};
   wire [63:0] data1;
   wire [63:0] dataD;
 
-  register_file i_rf(.clk(clk), .rs1(rs1), .rd(rd), .data1(data1), .wen(1), .dataD(dataD));
+  register_file i_rf(.clk(clk),.rs1(rs1),.rs2(0),.rd(rd),.wen(1),.dataD(dataD),.data1(data1),.data2(0));
   alu #(64) i_alu(.A(data1), .B(imm), .sel(4'd0), .res(dataD));
   always @(posedge clk) begin
     pc = pc + 4;
--- npc/vsrc/LemonPC/register_file.v
+++ npc/vsrc/LemonPC/register_file.v
@@ -1,10 +1,10 @@
 module register_file #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
   input clk,
-  input [DATA_WIDTH-1:0] dataD,
   input [ADDR_WIDTH-1:0] rs1,
   input [ADDR_WIDTH-1:0] rs2,
   input [ADDR_WIDTH-1:0] rd,
   input wen,
+  input [DATA_WIDTH-1:0] dataD,
   output [DATA_WIDTH-1:0] data1,
   output [DATA_WIDTH-1:0] data2
 );
