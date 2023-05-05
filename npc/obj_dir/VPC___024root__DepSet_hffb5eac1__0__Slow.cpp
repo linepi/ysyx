18caf268d41d656c64ad52552b143ec521d747e5
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-05 13:17:34 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:17:34 up  4:10,  1 user,  load average: 0.91, 0.51, 0.42

---
+++ npc/obj_dir/VPC___024root__DepSet_hffb5eac1__0__Slow.cpp
@@ -0,0 +1,80 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See VPC.h for the primary calling header
+
+#include "verilated.h"
+
+#include "VPC___024root.h"
+
+VL_ATTR_COLD void VPC___024root___eval_static(VPC___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    VPC__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VPC___024root___eval_static\n"); );
+}
+
+VL_ATTR_COLD void VPC___024root___eval_initial__TOP(VPC___024root* vlSelf);
+
+VL_ATTR_COLD void VPC___024root___eval_initial(VPC___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    VPC__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VPC___024root___eval_initial\n"); );
+    // Body
+    VPC___024root___eval_initial__TOP(vlSelf);
+}
+
+VL_ATTR_COLD void VPC___024root___eval_initial__TOP(VPC___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    VPC__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VPC___024root___eval_initial__TOP\n"); );
+    // Body
+    vlSelf->pc = 0x80000000ULL;
+}
+
+VL_ATTR_COLD void VPC___024root___eval_final(VPC___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    VPC__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VPC___024root___eval_final\n"); );
+}
+
+VL_ATTR_COLD void VPC___024root___eval_settle(VPC___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    VPC__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VPC___024root___eval_settle\n"); );
+}
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void VPC___024root___dump_triggers__act(VPC___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    VPC__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VPC___024root___dump_triggers__act\n"); );
+    // Body
+    if ((1U & (~ (IData)(vlSelf->__VactTriggered.any())))) {
+        VL_DBG_MSGF("         No triggers active\n");
+    }
+}
+#endif  // VL_DEBUG
+
+#ifdef VL_DEBUG
+VL_ATTR_COLD void VPC___024root___dump_triggers__nba(VPC___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    VPC__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VPC___024root___dump_triggers__nba\n"); );
+    // Body
+    if ((1U & (~ (IData)(vlSelf->__VnbaTriggered.any())))) {
+        VL_DBG_MSGF("         No triggers active\n");
+    }
+}
+#endif  // VL_DEBUG
+
+VL_ATTR_COLD void VPC___024root___ctor_var_reset(VPC___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    VPC__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VPC___024root___ctor_var_reset\n"); );
+    // Body
+    vlSelf->clk = VL_RAND_RESET_I(1);
+    vlSelf->inst = VL_RAND_RESET_I(32);
+    for (int __Vi0 = 0; __Vi0 < 32; ++__Vi0) {
+        vlSelf->regs[__Vi0] = VL_RAND_RESET_Q(64);
+    }
+    vlSelf->pc = VL_RAND_RESET_Q(64);
+}
