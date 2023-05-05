aace9e75782c3d8005bdef51dfb70c6ec61a3eb4
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-05 16:45:48 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:45:48 up  7:38,  1 user,  load average: 0.50, 0.54, 0.54

--- npc/vsrc/LemonPC/register_file.v
+++ npc/vsrc/LemonPC/register_file.v
@@ -12,7 +12,9 @@ module register_file #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
   always @(posedge clk) begin
     if (wen) begin 
       rf[rd] <= dataD;
-    end
+      for (i = 0; i < (1 << ADDR_WIDTH); i++) begin
+        $display("x%0d: %0d\n", i, rf[i]);
+      end
   end
   wire [63:0] ground = 64'b0;
   always @(*) rf[0] = ground;
@@ -28,7 +30,7 @@ module register_file #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
       rf_debug[rd] = dataD;
       for (i = 0; i < (1 << ADDR_WIDTH); i++) begin
         if (rf_debug_last[i] != rf_debug[i])
-          $display("x%0d changed, from 0x%0x(%0d) to 0x%0x(%0d)", i, rf_debug_last[i], $signed(rf_debug_last[i]), rf_debug[i], $signed(rf_debug[i]));
+          // $display("x%0d changed, from 0x%0x(%0d) to 0x%0x(%0d)", i, rf_debug_last[i], $signed(rf_debug_last[i]), rf_debug[i], $signed(rf_debug[i]));
       end
       rf_debug_last[rd] = rf_debug[rd];
     end
