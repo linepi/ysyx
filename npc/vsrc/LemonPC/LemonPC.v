92a837a63eca4b9fdbacb6b6b53d198dbf3b406a
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-05 14:51:44 +0800

    > sim RTL ysyx_22051216 吴桐 Linux x 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:51:44 up  5:44,  1 user,  load average: 0.46, 0.55, 0.46

--- npc/vsrc/LemonPC/LemonPC.v
+++ npc/vsrc/LemonPC/LemonPC.v
@@ -9,9 +9,10 @@ module PC (
   // signed extend
   wire [63:0] imm = {{52{inst[31]}}, inst[31:20]};
   wire [63:0] data1;
+  wire [63:0] data2;
   wire [63:0] dataD;
 
-  register_file #(5, 64) i_rf(.clk(clk),.rs1(rs1),.rs2(0),.rd(rd),.wen(1),.dataD(dataD),.data1(data1),.data2(0));
+  register_file #(5, 64) i_rf(.clk(clk),.rs1(rs1),.rs2(rs2),.rd(rd),.wen(1),.dataD(dataD),.data1(data1),.data2(data2));
   alu #(64) i_alu(.A(data1), .B(imm), .sel(4'd0), .res(dataD));
   always @(posedge clk) begin
     pc = pc + 4;
