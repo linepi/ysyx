f4d9c82c93959c0f03a4b6a5c1991e583a7542c7
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-06 14:45:14 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:45:14 up  5:28,  1 user,  load average: 0.36, 0.29, 0.34

--- nemu/src/isa/riscv64/npc/inst.cc
+++ nemu/src/isa/riscv64/npc/inst.cc
@@ -33,12 +33,10 @@ int isa_exec_once(Decode *s) {
 
   s->isa.inst.val = inst_fetch_add(&s->snpc, 4);
   PC->inst = s->isa.inst.val;
-  printf(ANSI_FMT("execute %016lx: %08x\n", ANSI_FG_GREEN), PC->pc, PC->inst);
   single_cycle();
   s->dnpc = PC->pc;
 
   if (PC->ebreak) {
-    printf(ANSI_FMT("Program hit ebreak\n", ANSI_FG_GREEN));
     NEMUTRAP(s->pc, R(10));
     clean_PC();
   }
