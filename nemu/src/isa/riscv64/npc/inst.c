e08ee48bef9b052f7705e4574bd7188b0dd07257
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-06 11:03:21 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:03:21 up  1:47,  1 user,  load average: 1.13, 1.10, 0.97

--- nemu/src/isa/riscv64/npc/inst.c
+++ nemu/src/isa/riscv64/npc/inst.c
@@ -14,6 +14,7 @@
 ***************************************************************************************/
 
 #include "../local-include/reg.h"
+#include "../include/decode.h"
 #include <cpu/cpu.h>
 #include <cpu/ifetch.h>
 #include <cpu/decode.h>
