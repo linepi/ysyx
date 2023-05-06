e406e44ec4b4da8a5b5bc4c48c77436095ad4e6d
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-06 12:23:00 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:23:00 up  3:06,  1 user,  load average: 1.91, 1.32, 1.13

--- nemu/scripts/npc.mk
+++ nemu/scripts/npc.mk
@@ -4,5 +4,20 @@ NPC_VSRCS = $(shell find $(NPC_HOME)/vsrc/common $(NPC_HOME)/vsrc/LemonPC -name 
 VERILATOR = verilator
 VERILATOR_FLAGS += -MMD --build -cc --timing -O3 --x-assign fast --x-initial fast --noassert
 
-INC_PATH += $(NEMU_HOME)/src/isa/riscv64/npc
+NPC_INC_PATH += $(NEMU_HOME)/src/isa/riscv64/npc
 
+# for verilator source file
+VERILATOR_INC_PATH = $(VERILATOR_ROOT)/include $(VERILATOR_ROOT)/include/vltstd .
+VERILATOR_CFLAGS = -MMD -Os -DVM_COVERAGE=0 -DVM_SC=0 -DVM_TRACE=0 -DVM_TRACE_FST=0 -DVM_TRACE_VCD=0 \
+	-faligned-new -fcf-protection=none \
+	-Wno-bool-operation -Wno-sign-compare -Wno-uninitialized -Wno-unused-but-set-variable \
+	-Wno-unused-parameter -Wno-unused-variable -Wno-shadow
+VERILATOR_CSRCS = verilated.cpp verilated_threads.cpp verilated_dpi.cpp
+VERILATOR_CSRCS := $(addprefix $(VERILATOR_ROOT), $(VERILATOR_CSRCS))
+
+NPC_INCFLAGS = $(addprefix -I, $(NPC_INC_PATH) $(VERILATOR_INC_PATH))
+
+NPC_OBJ_DIR = $(NEMU_HOME)/src/isa/riscv64/npc/obj_dir
+NPC_OBJS = $(patsubst %.cpp, %.o, $(addprefix $(NPC_OBJ_DIR), $(notdir $(NPC_CSRCS) $(VERILATOR_CSRCS))))
+
+$(info $(NPC_OBJS))
