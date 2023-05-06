41eec8ef27e105fc586f51848c874ee4dc1e67b5
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-06 14:20:24 +0800

    > build npc ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:20:24 up  5:04,  1 user,  load average: 0.36, 0.41, 0.43

--- npc/csrc/LemonPC/LemonPC.cpp
+++ npc/csrc/LemonPC/LemonPC.cpp
@@ -18,12 +18,12 @@ void single_cycle() {
 int main(int argc, char** argv, char** env) {
   VerilatedContext* contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
-  PC = new VPC{contextp};
+  PC = new VPC;
   
   int cnt = 0;
   srand((unsigned) time(NULL));
   PC->pc = MBASE;
-  while (!contextp->gotFinish()) {
+  while (1) {
     PC->inst = pmem_read(PC->pc, 4);
     printf(ANSI_FMT("execute %016lx: %08x\n", ANSI_FG_GREEN), PC->pc, PC->inst);
     single_cycle();
@@ -34,6 +34,6 @@ int main(int argc, char** argv, char** env) {
   }
   PC->final();
   delete PC;
-  delete contextp;
+  // delete contextp;
   return 0;
 }
