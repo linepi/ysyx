29862b84dcb0ed9e4701bd8b59f269793073e92d
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-06 16:52:22 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:52:22 up  7:35,  1 user,  load average: 0.92, 0.87, 0.76

--- nemu/src/isa/riscv64/npc/dpi.cc
+++ nemu/src/isa/riscv64/npc/dpi.cc
@@ -9,4 +9,6 @@ extern "C" {
 
 void write_r(const svLogicVecVal* no, const svLogicVecVal* val) {
 	gpr(no->aval) = val->aval;
+	printf("no->aval = %x, no->bval = %x\n", no->aval, no->bval);
+	printf("val->aval = %x, val->bval = %x\n", val->aval, val->bval);
 }
