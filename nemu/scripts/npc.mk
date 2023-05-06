fb84c8bbafde243d69c95521553367e85bda9bcd
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-06 14:07:30 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:07:30 up  4:51,  1 user,  load average: 0.57, 0.45, 0.45

--- nemu/scripts/npc.mk
+++ nemu/scripts/npc.mk
@@ -3,7 +3,7 @@ VERILATOR_FLAGS += -MMD --build -cc --timing -O3 --x-assign fast --x-initial fas
 
 # for verilator source file
 VERILATOR_INC_PATH = $(VERILATOR_ROOT)/include $(VERILATOR_ROOT)/include/vltstd .
-VERILATOR_CFLAGS = -MMD -Os -DVM_COVERAGE=0 -DVM_SC=0 -DVM_TRACE=0 -DVM_TRACE_FST=0 -DVM_TRACE_VCD=0 \
+VERILATOR_CFLAGS = -MMD -ggdb3 -Os -DVM_COVERAGE=0 -DVM_SC=0 -DVM_TRACE=0 -DVM_TRACE_FST=0 -DVM_TRACE_VCD=0 \
 	-faligned-new -fcf-protection=none \
 	-Wno-bool-operation -Wno-sign-compare -Wno-uninitialized -Wno-unused-but-set-variable \
 	-Wno-unused-parameter -Wno-unused-variable -Wno-shadow
@@ -14,7 +14,7 @@ VERILATOR_CFLAGS += $(addprefix -I, $(VERILATOR_INC_PATH))
 
 NPC_CSRCS = $(shell find $(NEMU_HOME)/src/isa/riscv64/npc -name "*.cc")
 NPC_VSRCS = $(shell find $(NPC_HOME)/vsrc/common $(NPC_HOME)/vsrc/LemonPC -name "*.v")
-NPC_INC_PATH += $(NEMU_HOME)/src/isa/riscv64/npc $(NEMU_HOME)/src/isa/riscv64/npc/obj_dir 
+NPC_INC_PATH += $(NEMU_HOME)/src/isa/riscv64/npc/obj_dir 
 NPC_INCFLAGS = $(addprefix -I, $(NPC_INC_PATH) $(VERILATOR_INC_PATH))
 
 NPC_OBJ_DIR = $(NEMU_HOME)/src/isa/riscv64/npc/obj_dir
