23a16a42a94abf85ffa2098222c8e14cc67cda81
linepi <2428513224@qq.com>
2023-05-07 14:39:17 +0800

    1

--- nemu/src/isa/riscv64/npc/inst.cc
+++ nemu/src/isa/riscv64/npc/inst.cc
@@ -1,27 +1,10 @@
-extern "C" {
-#include <utils.h>
-#include "../local-include/reg.h"
-#include <cpu/cpu.h>
-#include <cpu/ifetch.h>
-#include <cpu/decode.h>
-#include <generated/autoconf.h>
-}
-
-#include <VPC.h> 
 #include "defs.h"
 
 VPC *PC;
 static bool inited = false;
 bool npc_end = false;
-
-void init_PC() {
-  PC = new VPC;
-  PC->pc = CONFIG_MBASE;
-}
-void clean_PC() {
-  PC->final();
-  delete PC;
-}
+vaddr_t *pc;
+uint32_t *inst;
 
 void single_cycle() {
   PC->clk = 0; PC->eval();
@@ -29,14 +12,20 @@ void single_cycle() {
 }
 
 int isa_exec_once(Decode *s) {
-  if (!inited) { init_PC(); inited = true; }
-  s->isa.inst.val = inst_fetch_add(&s->snpc, 4);
-  PC->inst = s->isa.inst.val;
+  if (!inited) {
+    PC = new VPC;
+    inited = true; 
+  }
+
+  s->snpc += 4;
   single_cycle();
-  s->dnpc = PC->pc;
+  s->isa.inst.val = *inst;
+  s->dnpc = *pc;
+
   if (npc_end) {
     NEMUTRAP(cpu.pc, R(10));
-    clean_PC();
+    PC->final();
+    delete PC;
   }
   return 0;
 }
--- npc/vsrc/LemonPC/LemonPC.v
+++ npc/vsrc/LemonPC/LemonPC.v
@@ -1,11 +1,20 @@
 import "DPI-C" function void ebreak ();
 import "DPI-C" function void set_gpr_ptr(input logic [63:0] a []);
+import "DPI-C" function void npc_vmem_read(input longint raddr, output longint rdata);
+import "DPI-C" function void npc_vmem_write(input longint waddr, input longint wdata, input byte wmask);
+import "DPI-C" function void set_pc_inst_ptr(input logic [63:0] pc, input logic [31:0] inst);
     
 module PC (
-  input clk,
-  input [31:0] inst,
-  output reg [63:0] pc
+  input clk
 );
+  wire [31:0] inst;
+  wire [63:0] pc;
+  initial begin 
+    set_pc_inst_ptr(pc, inst);
+  end
+  wire [63:0] npc;
+  register #(64, 64'h0000000080000000) r_pc(.clk(clk), .rst(0), .din(npc), .dout(pc), .wen(1));
+
   wire [4:0] rs1 = inst[19:15];
   wire [4:0] rs2 = 0;
   wire [4:0] rd = inst[11:7];
@@ -15,12 +24,12 @@ module PC (
   wire [63:0] data2;
   wire [63:0] dataD;
   
-  control i_control(inst);
+  control i_control(clk, inst);
 
-  register_file #(5, 64) i_rf(clk, rs1, rs2, rd, wen, dataD, data1, data2);
-  alu #(64) i_alu(.A(data1), .B(imm), .sel(4'd0), .res(dataD));
+  register_file #(5, 64) r_rf(clk, rs1, rs2, rd, wen, dataD, data1, data2);
+  alu #(64) a(.A(data1), .B(imm), .sel(4'd0), .res(dataD));
+  alu #(64) a_pc(.A(pc), .B(64'd4), .sel(4'd0), .res(npc));
 
-  always @(posedge clk) begin
-    pc = pc + 4;
-  end
+  wire [31:0] nothing;
+  memory m_pc(.addr(pc), .wdata(64'd0), .wen(1'b0), .wmask(8'h0f), .rdata({nothing, inst}));
 endmodule
