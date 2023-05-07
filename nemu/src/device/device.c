830991c6d6f7be89ca3f43d3baa5080f2be60bf6
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-07 23:06:41 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:06:41 up 10:07,  1 user,  load average: 0.59, 0.50, 0.32

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -34,36 +34,36 @@ void send_key(uint8_t, bool);
 void vga_update_screen();
 
 void device_update() {
-  static uint64_t last = 0;
-  uint64_t now = get_time();
-  if (now - last < 1000000 / TIMER_HZ) {
-    return;
-  }
-  last = now;
+  // static uint64_t last = 0;
+  // uint64_t now = get_time();
+  // if (now - last < 1000000 / TIMER_HZ) {
+  //   return;
+  // }
+  // last = now;
 
   IFDEF(CONFIG_HAS_VGA, vga_update_screen());
 
-// #ifndef CONFIG_TARGET_AM
-//   SDL_Event event;
-//   while (SDL_PollEvent(&event)) {
-//     switch (event.type) {
-//       case SDL_QUIT:
-//         nemu_state.state = NEMU_QUIT;
-//         break;
-// #ifdef CONFIG_HAS_KEYBOARD
-//       // If a key was pressed
-//       case SDL_KEYDOWN:
-//       case SDL_KEYUP: {
-//         uint8_t k = event.key.keysym.scancode;
-//         bool is_keydown = (event.key.type == SDL_KEYDOWN);
-//         send_key(k, is_keydown);
-//         break;
-//       }
-// #endif
-//       default: break;
-//     }
-//   }
-// #endif
+#ifndef CONFIG_TARGET_AM
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_QUIT:
+        nemu_state.state = NEMU_QUIT;
+        break;
+#ifdef CONFIG_HAS_KEYBOARD
+      // If a key was pressed
+      case SDL_KEYDOWN:
+      case SDL_KEYUP: {
+        uint8_t k = event.key.keysym.scancode;
+        bool is_keydown = (event.key.type == SDL_KEYDOWN);
+        send_key(k, is_keydown);
+        break;
+      }
+#endif
+      default: break;
+    }
+  }
+#endif
 }
 
 void sdl_clear_event_queue() {
