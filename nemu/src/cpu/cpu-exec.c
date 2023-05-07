cafed621fadde6c2d8e46ab81d25ef154e6d75cf
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-07 13:36:37 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:36:37 up 37 min,  1 user,  load average: 0.32, 0.41, 0.38

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -80,42 +80,11 @@ static void exec_once(Decode *s, vaddr_t pc) {
   s->snpc = pc;
   isa_exec_once(s);
   cpu.pc = s->dnpc;
-  
-#ifdef CONFIG_ITRACE
-  char *p = s->logbuf;
-  p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
-  int ilen = s->snpc - s->pc;
-  int i;
-  uint8_t *inst = (uint8_t *)&s->isa.inst.val;
-  for (i = ilen - 1; i >= 0; i --) {
-    p += snprintf(p, 4, " %02x", inst[i]);
-  }
-  int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
-  int space_len = ilen_max - ilen;
-  if (space_len < 0) space_len = 0;
-  space_len = space_len * 3 + 1;
-  memset(p, ' ', space_len);
-  p += space_len;
-
-#ifndef CONFIG_ISA_loongarch32r
-  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-  disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
-      MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
-#else
-  p[0] = '\0'; // the upstream llvm does not support loongarch32r
-#endif
-#endif
 }
 
 static void execute(uint64_t n) {
   Decode s;
-  if (g_print_step && n)
-    IFDEF(CONFIG_ITRACE, printf(ANSI_FMT("Lines executed:\n", ANSI_FG_GREEN)));
   for (;n > 0; n --) {
-    // before execute, synchonize with npc
-    // which is need to synchonize, how to synchonize, is a problem
-    // once this is done, nothing to do with ftrace or itrace or something, 
-    // because they are only defined by mem or cpu or s.
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
