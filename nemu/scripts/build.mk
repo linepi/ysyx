20e01631930030ce5e570dec5c3ce7916f39d0a3
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-08 18:06:11 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:06:11 up  7:25,  1 user,  load average: 1.20, 0.81, 0.63

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -20,7 +20,7 @@ CXX := clang++
 else
 CXX := g++
 endif
-LD := g++
+LD := clang++
 INCLUDES = $(addprefix -I, $(INC_PATH))
 CFLAGS  := -O2 -MMD -Wall $(INCLUDES) $(CFLAGS)
 LDFLAGS := -O2 $(LDFLAGS) -fopenmp
