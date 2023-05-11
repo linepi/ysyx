b954b399fdc4c1bec3e0aeba711797f16e649564
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-11 11:10:34 +0800

    > build npc ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:10:34 up 50 min,  1 user,  load average: 1.58, 1.43, 1.02

--- npc/csrc/LemonPC/LemonPC.cpp
+++ npc/csrc/LemonPC/LemonPC.cpp
@@ -22,15 +22,8 @@ int main(int argc, char** argv, char** env) {
   
   int cnt = 0;
   srand((unsigned) time(NULL));
-  PC->pc = MBASE;
   while (!contextp->gotFinish()) {
-    PC->inst = pmem_read(PC->pc, 4);
-    printf(ANSI_FMT("execute %016lx: %08x\n", ANSI_FG_GREEN), PC->pc, PC->inst);
     single_cycle();
-    if (PC->ebreak) {
-      printf(ANSI_FMT("Program hit ebreak, stop.\n", ANSI_FG_GREEN));
-      break;
-    }
   }
   PC->final();
   delete PC;
