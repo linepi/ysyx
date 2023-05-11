9614d9401abcae5a640f7214d2e00cdc23404175
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-11 11:45:13 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:45:13 up  1:25,  1 user,  load average: 0.46, 0.64, 0.88

--- npc/vsrc/LemonPC/LemonPC.v
+++ npc/vsrc/LemonPC/LemonPC.v
@@ -14,8 +14,6 @@ module PC(input clk);
     set_ptr(pc, 0);
     set_ptr({32'd0, inst}, 1);
     rst = 1;
-    #10;
-    rst = 0;
   end
 
   wire [4:0] rs1 = inst[19:15];
@@ -27,14 +25,18 @@ module PC(input clk);
   wire [63:0] data1;
   wire [63:0] data2;
   wire [63:0] dataD;
+  wire [31:0] nothing;
   
   control i_control(clk, inst);
   register #(64, 64'h0000000080000000) r_pc(clk, rst, npc, pc, wen);
-
   register_file #(5, 64) r_rf(clk, rs1, rs2, rd, wen, dataD, data1, data2);
+
   alu #(64) a(.A(data1), .B(imm), .sel(4'd0), .res(dataD));
   alu #(64) a_pc(.A(pc), .B(64'd4), .sel(4'd0), .res(npc));
 
-  wire [31:0] nothing;
   memory m_pc(.addr(pc), .wdata(64'd0), .wen(1'b0), .wmask(8'h0f), .rdata({nothing, inst}));
+
+  always @(*) begin
+    rst = 0;
+  end
 endmodule
