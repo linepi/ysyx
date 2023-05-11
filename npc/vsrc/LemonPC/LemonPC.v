43060fadddfa140de16a349d91d385066fbe8ea2
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-11 12:29:49 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:29:49 up  2:10,  1 user,  load average: 0.81, 0.47, 0.45

--- npc/vsrc/LemonPC/LemonPC.v
+++ npc/vsrc/LemonPC/LemonPC.v
@@ -27,8 +27,4 @@ module PC(input clk, output [63:0] pc, output [31:0] inst);
 
   memory m_pc(.addr(pc), .wdata(64'd0), .wen(1'b0), .wmask(8'h0f), .rdata({nothing, inst}));
 
-  initial begin 
-    #1000
-    rst = 0;
-  end
 endmodule
