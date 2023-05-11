4d1bee4740d6f55a821b2d36b2a2d367a85b5bfa
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-11 13:55:55 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:55:55 up  3:36,  1 user,  load average: 1.25, 0.76, 0.51

--- npc/vsrc/LemonPC/LemonPC.v
+++ npc/vsrc/LemonPC/LemonPC.v
@@ -6,30 +6,33 @@ import "DPI-C" function void npc_vmem_write(input longint waddr, input longint w
 
 module PC(input clk, output reg [63:0] pc, output [31:0] inst);
   wire [63:0] npc;
+  wire [31:0] nothing;
+  // selects 
+  wire [2:0] imm_sel;
 
   wire [4:0] rs1 = inst[19:15];
-  wire [4:0] rs2 = 0;
+  wire [4:0] rs2 = inst[24:20];
   wire [4:0] rd = inst[11:7];
-  wire [63:0] imm = {{52{inst[31]}}, inst[31:20]}; // signed extend 
+  wire [63:0] imm; 
   wire wen = 1;
   reg rst = 1;
   wire [63:0] data1;
   wire [63:0] data2;
   wire [63:0] dataD;
-  wire [31:0] nothing;
   
-  control i_control(clk, inst);
+  
+  control i_control(.clk(clk), .inst(inst), .imm_sel(imm_sel));
   register_file #(5, 64) r_rf(clk, rs1, rs2, rd, wen, dataD, data1, data2);
 
   alu #(64) a(.A(data1), .B(imm), .sel(4'd0), .res(dataD));
   alu #(64) a_pc(.A(pc), .B(64'd4), .sel(4'd0), .res(npc));
-
   memory m_pc(.addr(pc), .wdata(64'd0), .wen(1'b0), .wmask(8'h0f), .rdata({nothing, inst}));
+
+  imm_gen i_imm_gen(inst, imm_sel, imm);
 
   initial begin
     pc = 64'h0000000080000000;
   end
-
   always @(posedge clk) begin
     pc <= npc;
   end
--- npc/vsrc/LemonPC/control.v
+++ npc/vsrc/LemonPC/control.v
@@ -1,10 +1,26 @@
 module control (
   input clk,
-  input [31:0] inst
+  input [31:0] inst,
+  output reg [2:0] imm_sel
 );
-  always @(posedge clk) begin
-    case (inst)
-      32'b0000000_00001_00000_000_00000_1110011: ebreak();
+  always @(*) begin
+    casez (inst)
+      32'b0000000_00001_00000_000_00000_1110011: begin // ebreak
+        ebreak(); 
+      end
+      32'bzzzzzzz_zzzzz_zzzzz_000_zzzzz_0010011: begin // addi
+        imm_sel = 3'b000;
+      end
+      32'bzzzzzzz_zzzzz_zzzzz_zzz_zzzzz_0010111: begin // auipc
+        imm_sel = 3'b011;
+      end
+      32'bzzzzzzz_zzzzz_zzzzz_zzz_zzzzz_0110111: begin // lui
+        imm_sel = 3'b011;
+      end
+      32'bzzzzzzz_zzzzz_zzzzz_011_zzzzz_0100011: begin // sd
+        imm_sel = 3'b001;  
+      end
+      default: $display("Unspecified control case!");
     endcase
   end
 endmodule
