99928f7e75094a8a7e03983eac5fb6dd39dc7b43
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-11 15:33:44 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:33:44 up  5:14,  1 user,  load average: 0.42, 0.27, 0.26

--- npc/vsrc/LemonPC/LemonPC.v
+++ npc/vsrc/LemonPC/LemonPC.v
@@ -7,50 +7,53 @@ import "DPI-C" function void npc_vmem_write(input longint waddr, input longint w
 
 
 module PC(input clk, output reg [63:0] pc, output [31:0] inst);
-  wire [63:0] snpc, dnpc;
   wire [31:0] nothing;
   // selects and flags
-  wire ebreak_flag, pc_sel, alu_a_sel, alu_b_sel;
+  wire ebreak_flag, pc_sel, alu_a_sel, alu_b_sel, 
+    reg_wen, mem_wen; 
+  wire [1:0] reg_w_sel;
   wire [2:0] imm_sel;
   wire [3:0] alu_sel;
+  wire [7:0] mem_mask;
 
   wire [4:0] rs1 = inst[19:15];
   wire [4:0] rs2 = inst[24:20];
   wire [4:0] rd = inst[11:7];
-  wire [63:0] imm; 
-  wire wen = 1;
-  reg rst = 1;
-  wire [63:0] reg1;
-  wire [63:0] reg2;
-  wire [63:0] regw;
-  wire [63:0] alu_a;
+  wire [63:0] imm, reg1, reg2, regw;
+
+  wire [63:0] mem_data;
+
+  wire [63:0] alu_a, alu_b, alu_res;
   mux_key #(2, 1, 64) alu_a_mux(alu_a, alu_a_sel, {
     `alu_a_sel_rs1, reg1,
     `alu_a_sel_pc, pc
   });
-  wire [63:0] alu_b;
   mux_key #(2, 1, 64) alu_b_mux(alu_b, alu_b_sel, {
     `alu_b_sel_rs2, reg2,
     `alu_b_sel_imm, imm
   });
-  wire [63:0] alu_res;
-
-  assign alu_a = reg1;
-  assign alu_b = imm;
-  assign regw = alu_res;
+  mux_key #(3, 2, 64) reg_w_mux(regw, reg_w_sel, {
+    `reg_w_sel_alu, alu_res,
+    `reg_w_sel_mem, mem_data,
+    `reg_w_sel_pc, snpc
+  });
   
   control i_control(
     .clk(clk), .inst(inst), .ebreak_flag(ebreak_flag), 
     .imm_sel(imm_sel), .pc_sel(pc_sel), .alu_sel(alu_sel),
-    .alu_a_sel(alu_a_sel), .alu_b_sel(alu_b_sel)
+    .alu_a_sel(alu_a_sel), .alu_b_sel(alu_b_sel),
+    .reg_wen(reg_wen), .mem_wen(mem_wen), .mem_mask(mem_mask),
+    .reg_w_sel(reg_w_sel)
   );
-  register_file #(5, 64) r_rf(clk, rs1, rs2, rd, wen, regw, reg1, reg2);
+  register_file #(5, 64) r_rf(clk, rs1, rs2, rd, reg_wen, regw, reg1, reg2);
 
   alu #(64) a(.A(alu_a), .B(alu_b), .sel(alu_sel), .res(alu_res));
-  memory m_pc(.addr(pc), .wdata(64'd0), .wen(1'b0), .wmask(8'h0f), .rdata({nothing, inst}));
-
   imm_gen i_imm_gen(inst, imm_sel, imm);
+  memory m_mem(.addr(alu_res), .wdata(reg2), .wen(mem_wen), .wmask(mem_mask), .rdata(mem_data));
 
+  // about pc and instruction 
+  wire [63:0] snpc, dnpc;
+  memory m_inst(.addr(pc), .wdata(64'd0), .wen(1'b0), .wmask(`mem_mask_read), .rdata({nothing, inst}));
   alu #(64) a_snpc(.A(pc), .B(64'd4), .sel(4'd0), .res(snpc));
   mux_key_with_default #(2, 1, 64) pc_mux(dnpc, pc_sel, `PC_INIT, {
     `pc_sel_snpc, snpc, 
--- npc/vsrc/LemonPC/control.v
+++ npc/vsrc/LemonPC/control.v
@@ -8,43 +8,67 @@ module control (
   output reg alu_a_sel,
   output reg alu_b_sel,
   output reg pc_sel,
-  output reg ebreak_flag
+  output reg ebreak_flag,
+  output reg reg_wen,
+  output reg mem_wen,
+  output reg [7:0] mem_mask,
+  output reg [1:0] reg_w_sel
 );
   always @(*) begin
-    ebreak_flag = 0;
+    ebreak_flag = `false;
+    reg_wen = `false;
+    mem_wen = `false;
     pc_sel = `pc_sel_snpc;
     casez (inst)
       32'b0000000_00001_00000_000_00000_1110011: begin // ebreak
-        ebreak_flag = 1;
+        ebreak_flag = `true;
       end
       32'bzzzzzzz_zzzzz_zzzzz_000_zzzzz_0010011: begin // addi(I)
         imm_sel = `imm_sel_I;
         alu_sel = `alu_sel_add;
         alu_a_sel = `alu_a_sel_rs1;
         alu_b_sel = `alu_b_sel_imm;
+        reg_wen = `true;
+        reg_w_sel = `reg_w_sel_alu;
       end
       32'bzzzzzzz_zzzzz_zzzzz_000_zzzzz_1100111: begin // jalr(I)
         imm_sel = `imm_sel_I;
         alu_sel = `alu_sel_add;
-        alu_a_sel = `alu_a_sel_pc;
+        alu_a_sel = `alu_a_sel_rs1;
+        alu_b_sel = `alu_b_sel_imm;
         pc_sel = `pc_sel_alu;
+        reg_wen = `true;
+        reg_w_sel = `reg_w_sel_pc;
       end
       32'bzzzzzzz_zzzzz_zzzzz_000_zzzzz_1100111: begin // jal(UJ)
         imm_sel = `imm_sel_UJ;
         alu_sel = `alu_sel_add;
+        alu_a_sel = `alu_a_sel_pc;
+        alu_b_sel = `alu_b_sel_imm;
         pc_sel = `pc_sel_alu;
+        reg_wen = `true;
+        reg_w_sel = `reg_w_sel_pc;
       end
       32'bzzzzzzz_zzzzz_zzzzz_zzz_zzzzz_0010111: begin // auipc(U)
         imm_sel = `imm_sel_U;
         alu_sel = `alu_sel_add;
         alu_a_sel = `alu_a_sel_pc;
         alu_b_sel = `alu_b_sel_imm;
+        reg_wen = `true;
+        reg_w_sel = `reg_w_sel_alu;
       end
       32'bzzzzzzz_zzzzz_zzzzz_zzz_zzzzz_0110111: begin // lui(U)
         imm_sel = `imm_sel_U;
+        reg_wen = `true;
+        reg_w_sel = `reg_w_sel_alu;
       end
       32'bzzzzzzz_zzzzz_zzzzz_011_zzzzz_0100011: begin // sd(S)
         imm_sel = `imm_sel_S;  
+        alu_sel = `alu_sel_add;
+        alu_a_sel = `alu_a_sel_rs1;
+        alu_b_sel = `alu_b_sel_imm;
+        mem_wen = `true;
+        mem_mask = `mem_mask_d;
       end
       default: $display("Unspecified control case!");
     endcase
