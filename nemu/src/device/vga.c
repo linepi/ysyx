940b2bb22ce0787a08e505d28a303f097124e1d3
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-13 22:02:52 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:02:52 up 10:17,  1 user,  load average: 0.68, 0.70, 0.53

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -46,10 +46,6 @@ static void init_screen() {
   char title[128];
   sprintf(title, "%s-NEMU", str(__GUEST_ISA__));
   SDL_Init(SDL_INIT_VIDEO);
-  // SDL_CreateWindowAndRenderer(
-  //     SCREEN_W * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
-  //     SCREEN_H * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
-  //     0, &window, &renderer);
   SDL_CreateWindowAndRenderer(
       SCREEN_W,
       SCREEN_H,
