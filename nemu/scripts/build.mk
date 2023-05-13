b1f881af7e2307c61e21bf5a6270178f96f119f8
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-13 20:27:31 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:27:31 up  8:41,  1 user,  load average: 0.29, 0.16, 0.05

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -22,8 +22,8 @@ CXX := g++
 endif
 LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
-CFLAGS  := -O2 -MMD -Wall $(INCLUDES) $(CFLAGS)
-LDFLAGS := -O2 $(LDFLAGS) 
+CFLAGS  := -O2 -MMD -Wall $(INCLUDES) $(CFLAGS) -pg
+LDFLAGS := -O2 $(LDFLAGS) -pg
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
