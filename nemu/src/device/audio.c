e39ffdd18875af1fdba72cb4e171661bef3f600b
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-15 19:31:09 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:31:09 up 10:26,  1 user,  load average: 0.65, 0.22, 0.27

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -28,11 +28,12 @@ enum {
 };
 
 #define FREQ_OFFSET 0
-#define CHANNELS_OFFSET 1
-#define SAMPLES_OFFSET 2
-#define SBUF_SIZE_OFFSET 3
-#define INIT_OFFSET 4
-#define COUNT_OFFSET 5
+#define CHANNELS_OFFSET 4
+#define SAMPLES_OFFSET 8
+#define SBUF_SIZE_OFFSET 12
+#define INIT_OFFSET 16
+#define COUNT_OFFSET 20
+#define OFFSET_DIV 4
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
@@ -40,21 +41,21 @@ static uint32_t *audio_base = NULL;
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
   switch (offset) {
     case FREQ_OFFSET:
-      printf("device register -- freq writed with %d\n", audio_base[FREQ_OFFSET]);
+      printf("device register -- freq writed with %d\n", audio_base[FREQ_OFFSET / OFFSET_DIV]);
       break;
     case CHANNELS_OFFSET:
-      printf("device register -- channels writed with %d\n", audio_base[CHANNELS_OFFSET]);
+      printf("device register -- channels writed with %d\n", audio_base[CHANNELS_OFFSET / OFFSET_DIV]);
       break;
     case SAMPLES_OFFSET:
-      printf("device register -- samples writed with %d\n", audio_base[SAMPLES_OFFSET]);
+      printf("device register -- samples writed with %d\n", audio_base[SAMPLES_OFFSET / OFFSET_DIV]);
       break;
     case SBUF_SIZE_OFFSET:
-      audio_base[SBUF_SIZE_OFFSET] = CONFIG_SB_ADDR;
+      audio_base[SBUF_SIZE_OFFSET / OFFSET_DIV] = CONFIG_SB_ADDR;
       break;
     case INIT_OFFSET:
       break;
     case COUNT_OFFSET:
-      audio_base[COUNT_OFFSET] = 0;
+      audio_base[COUNT_OFFSET / OFFSET_DIV] = 0;
       break;
     default: panic("device/Audio.c: do not support offset = %d", offset);
   }
