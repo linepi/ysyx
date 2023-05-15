3269bbd66262e5f93b55401d0efc1fd2fe2735fd
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-15 22:01:14 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:01:14 up 12:56,  1 user,  load average: 0.66, 0.42, 0.39

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -95,7 +95,7 @@ static void audio_sbuf_io_handler(uint32_t offset, int len, bool is_write) {
 }
 
 static void audioCallback(void* userdata, Uint8* stream, int len) {
-  for (int i = 0; i < len; i++) {
+  for (int i = 0; i < len / 2; i++) {
     stream[i] = R_SBUF;
   }
 }
@@ -122,5 +122,4 @@ void init_audio() {
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, audio_sbuf_io_handler);
 
   init_SDL_AudioSpec();
-  printf("%u %u %u\n", sbuf_l, sbuf_r, sbuf_count);
 }
