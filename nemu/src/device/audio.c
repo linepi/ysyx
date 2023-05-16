75759fa857db4a26d63f467f88f5d666e5675187
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-16 11:40:59 +0800

    > compile NEMU ysyx_22051216 吴桐 Linux x 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:40:59 up 12:05,  1 user,  load average: 1.11, 1.22, 1.31

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -124,7 +124,6 @@ static void audioCallback(void* userdata, Uint8* stream, int len) {
   for (int i = 0; i < len; i++) {
     uint8_t t = R_SBUF;
     stream[i] = t;
-    printf("%u ", t);
   }
 }
 
